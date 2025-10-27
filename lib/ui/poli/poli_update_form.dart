3740b29acfde338eb45a4401a89bed88eabeeeee
Fadriasya <nasrullahakbar18@gmail.com>
2025-10-27 23:27:55 +0700

    terbaru

--- lib/ui/poli/poli_update_form.dart
+++ lib/ui/poli/poli_update_form.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import '/model/poli.dart';
 import '/service/poli_service.dart';
-import 'poli_detail.dart';
 
 class PoliUpdateForm extends StatefulWidget {
   final Poli poli;
